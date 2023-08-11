b66a92cda2b5b1d0865e23d9aa62e00a7f1d4a97
Renata Fernandes de Franca Carneiro <renatactba@hotmail.com>
2023-08-11 20:53:14 -0300

    Banner e Formulario

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Banner from "./componentes/banner"
+import Formulario from "./componentes/Formulario";
+
+function App() {
+  return (
+    <div className="App">   
+       <Banner/>  
+       <Formulario/>
+    </div>
+  );
+}
+
+export default App;
